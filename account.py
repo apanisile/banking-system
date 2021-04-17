75d30109eb1d33fb3a6bf7d3d76d7269f6189af6
apanisile <apanisileifeoluwa@gmail.com>
2021-04-17 13:35:29 +0100

    Fixed the account generation

---
+++ account.py
@@ -0,0 +1,5 @@
+import random
+
+def generating_account():
+    account_number = random.randrange(1111, 9999)
+    return account_number
--- main.py
+++ main.py
@@ -1,2 +1,48 @@
+import account as ac
+
+database = {}
+
+
+
+
 def init():
-    print("Hi! Welcome to Osokoya Banking ATM")
+    print("Do you have an account with us? 1 (Yes) 2 (No)")
+    answerRequest = int(input("> "))
+    if (answerRequest == 1):
+        haveAccount = True
+        login()
+    elif (answerRequest == 2):
+        haveAccount = True
+        register()
+    else:
+        print("You dont have an account!")
+
+
+def register():
+    fname = input("Please enter your first name: \n")
+    lname = input("Enter your last name: \n")
+    email = input("Enter your email: \n")
+    password = input("Enter your pasword: \n")
+
+    account_no = ac.generating_account()
+
+    print(account_no)
+
+    database[account_no] = [fname, lname, email, password]
+    print(f"{fname}{lname} your account has been successful created!")
+    print("You can now login")
+    login(account_no)
+
+
+def login(account_no):
+    print("=======================================")
+    print("Registration Successful")
+    print("========================================")
+    print(f"Your account number is : {account_no}")
+    accountFromUser = int(input("Enter your account number"))
+    if accountFromUser != account_no:
+        print("Sorry! No account found")
+
+
+######## ACTUAL BANKING #######
+init()
